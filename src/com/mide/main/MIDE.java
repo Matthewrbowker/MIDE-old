b92cac5a37acc8f2a597d4406910a272c04c47b4
Matthew Bowker <matthewrbowker@me.com>
2015-05-21 21:29:42 -0600

    Interface operations

--- src/com/mide/main/MIDE.java
+++ src/com/mide/main/MIDE.java
@@ -2,13 +2,14 @@
 package com.mide.main;
 import com.mide.tests.*;
 import com.mide.view.*;
+import javax.swing.JFrame;
 
 public class MIDE {
   public static void main(String... args) {
       if (args.length > 0 && args[0].toLowerCase().equals("test")) {
           Test_Bootstrapper boot = new Test_Bootstrapper();
           boot.execute();
-         
+          // boostrapper will exit the execution based on test results
       }
       
       /*
@@ -30,6 +31,12 @@ public class MIDE {
       s.setSetting("test.String2", "Testing2");
       s.printAllSettings();
       */
-      System.out.println("Execute main interface");
+      System.out.println("Execute main interface...");
+      JFrame frame = new JFrame();
+      frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+      frame.getContentPane().add(new com.mide.view.Main());
+      frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+      frame.pack();
+      frame.setVisible(true);
   }
 }
